9e26a4c1aa78e987dcd135eca3d93733e238ee1c
Dino Tahto <dinotahto.dev@gmail.com>
2020-09-08 17:40:55 +0200

    Add oh-my-zsh install

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@
 #DOTFILES SETUP#
 ################
 
+
 #Creates a backup of current dotfiles in ~/.dotfiles_backup/$now, removes existing symlinks and creates symlinks for new dotfiles
 echo "Dotfiles Setup"
 read -r -p "symlink all dotfiles to ~? [y|N] " response
@@ -32,3 +33,15 @@ if [[ $response =~ (y|yes|Y) ]]; then
   done
 fi
 
+echo "Oh-My-Zsh Setup"
+read -r -p "Install oh-my-zsh? [y|N] " response
+if [[ $response =~ (y|yes|Y) ]]; then
+	echo "Cloning oh-my-zsh"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	echo "Cloning zsh-syntax-highlighting"
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	echo "Cloning zsh-completions"
+	git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+	echo "Cloning zsh-autosuggestions"
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
