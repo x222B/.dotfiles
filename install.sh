78cdf53879112b7acc484b92cabb67eb45118923
Dino Tahto <dinotahto.dev@gmail.com>
2021-01-02 11:22:38 +0100

    Add highlights to messages

--- install.sh
+++ install.sh
@@ -5,12 +5,15 @@
 #DOTFILES SETUP#
 ################
 
+highlight() {
+    echo "[1;34m::[0m [1m$*[0m"
+}
 
 #Creates a backup of current dotfiles in ~/.dotfiles_backup/$now, removes existing symlinks and creates symlinks for new dotfiles
-echo "Dotfiles Setup"
+highlight "Dotfiles Setup"
 read -r -p "symlink all dotfiles to ~? [y|N] " response
 if [[ $response =~ (y|yes|Y) ]]; then
-  echo "Creating symlinks for dotfiles..."
+  highlight "Creating symlinks for dotfiles..."
   now=$(date +"%Y.%m.%d.%H.%M.%S")
 
   for file in .*; do
@@ -23,32 +26,32 @@ if [[ $response =~ (y|yes|Y) ]]; then
     if [[ -e ~/$file ]]; then
         mkdir -p ~/.dotfiles_backup/$now
         mv ~/$file ~/.dotfiles_backup/$now/$file
-        echo "backup saved as ~/.dotfiles_backup/$now/$file"
+        highlight "backup saved as ~/.dotfiles_backup/$now/$file"
     fi
     # symlink might still exist
     unlink ~/$file > /dev/null 2>&1
     # create the link
     ln -s ~/.dotfiles/$file ~/$file
-    echo -en '\tlinked';ok
+    highlight -en '\tlinked';ok
   done
 fi
 
-echo "Oh-My-Zsh Setup"
+highlight "Oh-My-Zsh Setup"
 read -r -p "Install oh-my-zsh? [y|N] " response
 if [[ $response =~ (y|yes|Y) ]]; then
-	echo "Cloning oh-my-zsh"
+	highlight "Cloning oh-my-zsh"
 	ZSH=~.config/zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-	echo "Cloning zsh-syntax-highlighting"
+	highlight "Cloning zsh-syntax-highlighting"
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 	${ZSH_CUSTOM:-~/.config/zsh/custom}/plugins/zsh-syntax-highlighting
-	echo "Cloning zsh-completions"
+	highlight "Cloning zsh-completions"
 	git clone https://github.com/zsh-users/zsh-completions
 	${ZSH_CUSTOM:=~/.config/zsh/custom}/plugins/zsh-completions
-	echo "Cloning zsh-autosuggestions"
+	highlight "Cloning zsh-autosuggestions"
 	git clone https://github.com/zsh-users/zsh-autosuggestions
 	${ZSH_CUSTOM:-~/.config/zsh/custom}/plugins/zsh-autosuggestions
-	echo "Copying theme"
+	highlight "Copying theme"
 	cp zsh/themes/lambda.zsh-theme ~/.config/zsh/custom/themes
-	echo "Copying Settings"
+	highlight "Copying Settings"
 	cp zsh/*.zsh ~/.config/zsh
 fi
